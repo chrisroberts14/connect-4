5e59cbfe3c47a91a259738730fa00a45ebcd5c23
Chris <chrisroberts1080@gmail.com>
2024-12-17 15:44:15 +0000

    Add cmake

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 // Entry into the game
+
+#include "game.h"
 int main(){
   // Create the game and run it with the default settings
+  game g(std::make_unique<player>("Player 1"), std::make_unique<player>("Player 2"));
+  g.run();
   return 0;
 }
